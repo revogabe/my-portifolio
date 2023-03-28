28de643d23dad36bbdcc88acdf779aa8192820b2
Daniel Gabriel <reelpscanal@gmail.com>
2023-03-27 23:26:08 -0300

    loading

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -8,6 +8,7 @@ export function Star() {
   const [isStarred, setIsStarred] = useState('')
   const [starOn, setStarOn] = useState(false)
   const [starsCount, setStarCount] = useState('')
+  const [loading, setLoading] = useState(true)
 
   const stars = fetch('/api/stars')
     .then((res) => res.json())
@@ -22,11 +23,14 @@ export function Star() {
     if (token) {
       setIsStarred(token)
     }
+    setTimeout(() => {
+      setLoading(false)
+    }, 500)
   }, [])
 
   async function handleAddStars() {
     if (isStarred !== 'true' && starOn === false) {
-      await fetch('/api/stars', {
+      fetch('/api/stars', {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json',
@@ -43,22 +47,32 @@ export function Star() {
         path: '/',
       })
       setStarOn(true)
+      setLoading(false)
     } else {
       return null
     }
   }
 
   return (
-    <button
-      className="flex items-center gap-2 rounded-md transition-all duration-200 ease-out hover:bg-zinc-900 hover:py-2 hover:px-4"
-      onClick={handleAddStars}
-    >
-      {isStarred || starOn === true ? (
-        <StarSolid className="text-emerald-400" width={28} height={28} />
+    <div>
+      {loading ? (
+        <button className="animate-pulse flex items-center gap-2 rounded-md transition-all duration-200 ease-out bg-zinc-900 py-2 px-4">
+          <StarSolid className="text-zinc-800/75" width={28} height={28} />
+          <div className="flex items-center gap-2 rounded-md transition-all duration-200 ease-out bg-zinc-800/75 py-3 px-4" />
+        </button>
       ) : (
-        <StarIcon className="text-emerald-400" width={28} height={28} />
+        <button
+          className="flex items-center gap-2 rounded-md transition-all duration-200 ease-out hover:bg-zinc-900 hover:py-2 hover:px-4"
+          onClick={handleAddStars}
+        >
+          {isStarred || starOn === true ? (
+            <StarSolid className="text-emerald-400" width={28} height={28} />
+          ) : (
+            <StarIcon className="text-emerald-400" width={28} height={28} />
+          )}
+          <p className="text-lg font-bold text-zinc-200">{starsCount}</p>
+        </button>
       )}
-      <p className="text-lg font-bold text-zinc-200">{starsCount}</p>
-    </button>
+    </div>
   )
 }
